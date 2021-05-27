2e29df7b504583b8399f01ab3c2f8ecc12e6221d
RedBlazerFlame <gabeedevera@outlook.com>
2021-05-27 14:21:31 +0800

    Fix server timeout clearing issues

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const buttonTextStatus = ["Purple", "Blue", "Green", "Yellow", "Orange", "Red"];
 const buttonDelay = [6, 5, 4, 3, 3, 2].map((item) => item * 60 * 60 * 1000);
 let buttonTimeoutLoop;
 let buttonNumericalStatus = -1;
+let buttonIsClickable = true;
 // Setting up the web server
 const httpServer = http.createServer((_, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -28,6 +29,8 @@ const httpServer = http.createServer((_, res) => {
 });
 /// Function to update the button status
 function updateStatus(buttonStatusChannel) {
+    // Make the button un-clickable
+    buttonIsClickable = false;
     // Increment button status
     buttonNumericalStatus++;
     // If button is dead, reset; else, send button status
@@ -35,16 +38,48 @@ function updateStatus(buttonStatusChannel) {
         // Take away button roles
         const sampaGuild = client.guilds.cache.get(sampaServerId);
         sampaGuild === null || sampaGuild === void 0 ? void 0 : sampaGuild.members.fetch().then((members) => {
-            members.forEach((member) => {
-                buttonTextStatus.forEach((colorName) => {
+            // Filtering the members to only include the ones with a belt
+            let filteredMembers = members.filter((member) => buttonTextStatus.reduce((acc, cur) => {
+                const roleName = `${cur} Belt`;
+                const role = sampaGuild === null || sampaGuild === void 0 ? void 0 : sampaGuild.roles.cache.find((item) => item.name === roleName);
+                return acc || member.roles.cache.has(role.id);
+            }, false));
+            // If there are no people with belts, call the function again
+            if ([...filteredMembers].length === 0) {
+                setTimeout(updateStatus, 1000, buttonStatusChannel);
+            }
+            [...filteredMembers.values()].forEach((member, memberIndex) => {
+                // Stores the index of the color role that the user has
+                let roleIndex = 0;
+                buttonTextStatus.forEach((colorName, index) => {
                     const roleName = `${colorName} Belt`;
                     const role = sampaGuild === null || sampaGuild === void 0 ? void 0 : sampaGuild.roles.cache.find((item) => item.name === roleName);
-                    member.roles.remove(role).then((value) => {
-                        console.log(`Removed role for ${value.user.username}`);
-                    }).catch(err => {
-                        console.log(err);
-                    });
+                    roleIndex = (member.roles.cache.has(role.id) ? index : roleIndex);
+                    if (member.roles.cache.has(role.id)) {
+                        member.roles.remove(role).then((value) => {
+                            console.log(`Removed role ${roleName} for ${value.user.username}`);
+                        }).catch(err => {
+                            console.log(err);
+                        }).finally(() => {
+                            // If the person whose role was updated was the last person, call the function again
+                            if ([...filteredMembers.values()].length - 1 === memberIndex) {
+                                console.log("This is the last person, call next loop");
+                                setTimeout((statusChannel) => {
+                                    updateStatus(statusChannel);
+                                    buttonIsClickable = true;
+                                }, 1000, buttonStatusChannel);
+                            }
+                        });
+                    }
                 });
+                // If the user has a blue belt or higher, give them the belt that is one level lower than their previous belt
+                if (roleIndex - 1 >= 0) {
+                    const roleName = `${buttonTextStatus[roleIndex - 1]} Belt`;
+                    const role = sampaGuild === null || sampaGuild === void 0 ? void 0 : sampaGuild.roles.cache.find((item) => item.name === roleName);
+                    member.roles.add(role).then((value) => {
+                        console.log(`Added role ${roleName} for ${value.user.username}`);
+                    });
+                }
             });
         }).catch(err => {
             console.log(err);
@@ -52,14 +87,12 @@ function updateStatus(buttonStatusChannel) {
         // Notify Members that the button died
         const embedMessage = new Discord.MessageEmbed()
             .setTitle("**Button Status**")
-            .setDescription(`Oh no! The button is dead! Your belts have now been taken`)
+            .setDescription(`Oh no! The button is dead! Your belts have now been downgraded.`)
             .setColor("WHITE");
         buttonStatusChannel.messages.fetch().then((messages) => {
             let messageList = [...messages.filter(msg => msg.author.id === botId).values()];
             buttonStatusChannel.send(embedMessage).then(() => {
-                messageList[0].edit(embedMessage).then(_ => {
-                    buttonTimeoutLoop = setTimeout(updateStatus, buttonDelay[buttonNumericalStatus], buttonStatusChannel);
-                });
+                messageList[0].edit(embedMessage);
             });
         });
         buttonNumericalStatus = -1;
@@ -72,13 +105,15 @@ function updateStatus(buttonStatusChannel) {
         buttonStatusChannel.messages.fetch().then((messages) => {
             let messageList = [...messages.filter(msg => msg.author.id === botId).values()];
             if (messageList.length === 0) {
-                buttonStatusChannel.send(embedMessage).then(_ => {
+                buttonStatusChannel.send(embedMessage).then(() => {
                     buttonTimeoutLoop = setTimeout(updateStatus, buttonDelay[buttonNumericalStatus], buttonStatusChannel);
+                    buttonIsClickable = true;
                 });
             }
             else {
-                messageList[0].edit(embedMessage).then(_ => {
+                messageList[0].edit(embedMessage).then(() => {
                     buttonTimeoutLoop = setTimeout(updateStatus, buttonDelay[buttonNumericalStatus], buttonStatusChannel);
+                    buttonIsClickable = true;
                 });
             }
         });
@@ -92,7 +127,6 @@ client.on('ready', () => {
     const buttonStatusChannel = client.channels.cache.get(buttonStatusChannelId);
     updateStatus(buttonStatusChannel);
 });
-// Setting Up "The Button" Code
 // Setting up Commands
 /// Test command to see if the bot is up
 command(client, ["ping", "ping2"], (message) => {
@@ -100,39 +134,52 @@ command(client, ["ping", "ping2"], (message) => {
 });
 /// Click the button
 command(client, ["button click", "bc"], (message) => {
-    // Check if the user is in the right text channel
-    if (message.channel.id === buttonGeneralChannelId) {
+    // Check if the user is in the right text channel and if the button is clickable
+    if (message.channel.id !== buttonGeneralChannelId) {
+        message.channel.send("This is the wrong text channel to use this command. Please go to #button-general so that you can use this command. Thank you :>");
+    }
+    else if (!buttonIsClickable) {
+        message.channel.send("It seems like SampaBot is still processing some information (may take up to a few seconds). Please wait for him to finish before clicking the button again :>");
+    }
+    else {
+        buttonIsClickable = false;
+        // Reset the Button status
+        clearTimeout(buttonTimeoutLoop);
+        console.log(buttonTimeoutLoop);
         // Change roles
+        let newRoleColor = buttonTextStatus[buttonNumericalStatus];
         buttonTextStatus.forEach((colorName) => {
             var _a, _b;
             const roleName = `${colorName} Belt`;
             const role = (_a = message.guild) === null || _a === void 0 ? void 0 : _a.roles.cache.find((item) => item.name === roleName);
             const member = (_b = message.guild) === null || _b === void 0 ? void 0 : _b.members.cache.find((item) => item.id === message.author.id);
             // Check if the current color is the same as the color that is being checked for
-            if (colorName === buttonTextStatus[buttonNumericalStatus]) {
+            if (colorName === newRoleColor) {
                 member.roles.add(role);
             }
             else {
-                member.roles.remove(role);
+                if (member.roles.cache.has(role.id)) {
+                    member.roles.remove(role);
+                }
             }
         });
         // Tell the user that he got the button
-        message.channel.send(`Congratulations, ${message.author.username}! You got a ${buttonTextStatus[buttonNumericalStatus]} belt!`);
-        // Reset the Button status
-        clearTimeout(buttonTimeoutLoop);
+        message.channel.send(`Congratulations, ${message.author.username}! You got a ${newRoleColor} belt!`);
+        console.log(`Congratulations, ${message.author.username}! You got a ${newRoleColor} belt!`);
         /// Getting a reference to the button status channel
         const buttonStatusChannel = client.channels.cache.get(buttonStatusChannelId);
         const embedMessage = new Discord.MessageEmbed()
             .setTitle("**Button Status**")
-            .setDescription(`${message.author.username} has saved the button and earned a ${buttonTextStatus[buttonNumericalStatus]} belt!`)
+            .setDescription(`${message.author.username} has saved the button and earned a ${newRoleColor} belt!`)
             .setColor("WHITE");
         buttonStatusChannel.messages.fetch().then((messages) => {
             let messageList = [...messages.filter(msg => msg.author.id === botId).values()];
-            buttonStatusChannel.send(embedMessage).then(() => {
-                messageList[0].edit(embedMessage);
-                /// Resetting the button status
-                buttonNumericalStatus = -1;
-                updateStatus(buttonStatusChannel);
+            buttonStatusChannel.send(embedMessage).then(_ => {
+                messageList[0].edit(embedMessage).then(_ => {
+                    /// Resetting the button status
+                    buttonNumericalStatus = -1;
+                    updateStatus(buttonStatusChannel);
+                });
             });
         });
     }
--- server.ts
+++ server.ts
@@ -31,6 +31,7 @@ const buttonTextStatus: Array<string> = ["Purple", "Blue", "Green", "Yellow", "O
 const buttonDelay: Array<number> = [6, 5, 4, 3, 3, 2].map((item: number): number => item * 60 * 60 * 1000);
 let buttonTimeoutLoop: NodeJS.Timeout;
 let buttonNumericalStatus: number = -1;
+let buttonIsClickable: boolean = true;
 
 // Setting up the web server
 const httpServer: Http2Server = http.createServer((_: Http2ServerRequest, res: Http2ServerResponse) => {
@@ -41,6 +42,9 @@ const httpServer: Http2Server = http.createServer((_: Http2ServerRequest, res: H
 
 /// Function to update the button status
 function updateStatus(buttonStatusChannel: TextChannel): void {
+    // Make the button un-clickable
+    buttonIsClickable = false;
+
     // Increment button status
     buttonNumericalStatus++;
 
@@ -49,16 +53,52 @@ function updateStatus(buttonStatusChannel: TextChannel): void {
         // Take away button roles
         const sampaGuild = client.guilds.cache.get(sampaServerId);
         sampaGuild?.members.fetch().then((members) => {
-            members.forEach((member: GuildMember): void => {
-                buttonTextStatus.forEach((colorName: string): void => {
+            // Filtering the members to only include the ones with a belt
+            let filteredMembers = members.filter((member: GuildMember) => buttonTextStatus.reduce((acc: boolean, cur: string) => {
+                const roleName = `${cur} Belt`;
+                const role: Role = sampaGuild?.roles.cache.find((item: Role): boolean => item.name === roleName) as Role;
+                return acc || member.roles.cache.has(role.id);
+            }, false));
+
+            // If there are no people with belts, call the function again
+            if ([...filteredMembers].length === 0) {
+                setTimeout(updateStatus, 1000, buttonStatusChannel);
+            }
+
+            [...filteredMembers.values()].forEach((member: GuildMember, memberIndex: number): void => {
+                // Stores the index of the color role that the user has
+                let roleIndex = 0;
+
+                buttonTextStatus.forEach((colorName: string, index: number): void => {
                     const roleName = `${colorName} Belt`;
                     const role: Role = sampaGuild?.roles.cache.find((item: Role): boolean => item.name === roleName) as Role;
-                    member.roles.remove(role).then((value) => {
-                        console.log(`Removed role for ${value.user.username}`)
-                    }).catch(err => {
-                        console.log(err);
-                    });
+                    roleIndex = (member.roles.cache.has(role.id) ? index : roleIndex);
+                    if (member.roles.cache.has(role.id)) {
+                        member.roles.remove(role).then((value) => {
+                            console.log(`Removed role ${roleName} for ${value.user.username}`)
+                        }).catch(err => {
+                            console.log(err);
+                        }).finally(() => {
+                            // If the person whose role was updated was the last person, call the function again
+                            if ([...filteredMembers.values()].length - 1 === memberIndex) {
+                                console.log("This is the last person, call next loop");
+                                setTimeout((statusChannel: TextChannel) => {
+                                    updateStatus(statusChannel);
+                                    buttonIsClickable = true;
+                                }, 1000, buttonStatusChannel);
+                            }
+                        });
+                    }
                 });
+
+                // If the user has a blue belt or higher, give them the belt that is one level lower than their previous belt
+                if (roleIndex - 1 >= 0) {
+                    const roleName = `${buttonTextStatus[roleIndex - 1]} Belt`;
+                    const role: Role = sampaGuild?.roles.cache.find((item: Role): boolean => item.name === roleName) as Role;
+                    member.roles.add(role).then((value) => {
+                        console.log(`Added role ${roleName} for ${value.user.username}`);
+                    });
+                }
             })
         }).catch(err => {
             console.log(err)
@@ -67,16 +107,13 @@ function updateStatus(buttonStatusChannel: TextChannel): void {
         // Notify Members that the button died
         const embedMessage: MessageEmbed = new Discord.MessageEmbed()
             .setTitle("**Button Status**")
-            .setDescription(`Oh no! The button is dead! Your belts have now been taken`)
+            .setDescription(`Oh no! The button is dead! Your belts have now been downgraded.`)
             .setColor("WHITE");
 
         buttonStatusChannel.messages.fetch().then((messages) => {
             let messageList: Array<Message> = [...messages.filter(msg => msg.author.id === botId).values()];
             buttonStatusChannel.send(embedMessage).then(() => {
-                messageList[0].edit(embedMessage).then(_ => {
-                    buttonTimeoutLoop = setTimeout(updateStatus, buttonDelay[buttonNumericalStatus], buttonStatusChannel) as unknown as NodeJS.Timeout;
-                });
-
+                messageList[0].edit(embedMessage);
             });
         })
         buttonNumericalStatus = -1;
@@ -89,13 +126,16 @@ function updateStatus(buttonStatusChannel: TextChannel): void {
         buttonStatusChannel.messages.fetch().then((messages) => {
             let messageList: Array<Message> = [...messages.filter(msg => msg.author.id === botId).values()];
             if (messageList.length === 0) {
-                buttonStatusChannel.send(embedMessage).then(_ => {
+                buttonStatusChannel.send(embedMessage).then(() => {
                     buttonTimeoutLoop = setTimeout(updateStatus, buttonDelay[buttonNumericalStatus], buttonStatusChannel) as unknown as NodeJS.Timeout;
+                    buttonIsClickable = true;
                 });
+
             }
             else {
-                messageList[0].edit(embedMessage).then(_ => {
+                messageList[0].edit(embedMessage).then(() => {
                     buttonTimeoutLoop = setTimeout(updateStatus, buttonDelay[buttonNumericalStatus], buttonStatusChannel) as unknown as NodeJS.Timeout;
+                    buttonIsClickable = true;
                 });
             }
         })
@@ -113,8 +153,6 @@ client.on('ready', () => {
     updateStatus(buttonStatusChannel);
 });
 
-// Setting Up "The Button" Code
-
 
 // Setting up Commands
 /// Test command to see if the bot is up
@@ -124,45 +162,57 @@ command(client, ["ping", "ping2"], (message: Message): void => {
 
 /// Click the button
 command(client, ["button click", "bc"], (message: Message): void => {
-    // Check if the user is in the right text channel
-    if (message.channel.id === buttonGeneralChannelId) {
+    // Check if the user is in the right text channel and if the button is clickable
+    if (message.channel.id !== buttonGeneralChannelId) {
+        message.channel.send("This is the wrong text channel to use this command. Please go to #button-general so that you can use this command. Thank you :>");
+    }
+    else if (! buttonIsClickable) {
+        message.channel.send("It seems like SampaBot is still processing some information (may take up to a few seconds). Please wait for him to finish before clicking the button again :>")
+    }
+    else {
+        buttonIsClickable = false;
+        // Reset the Button status
+        clearTimeout(buttonTimeoutLoop);
+        console.log(buttonTimeoutLoop);
+
         // Change roles
+        let newRoleColor = buttonTextStatus[buttonNumericalStatus];
         buttonTextStatus.forEach((colorName: string): void => {
             const roleName = `${colorName} Belt`;
             const role: Role = message.guild?.roles.cache.find((item: Role): boolean => item.name === roleName) as Role;
             const member: GuildMember = message.guild?.members.cache.find((item: GuildMember): boolean => item.id === message.author.id) as GuildMember;
 
             // Check if the current color is the same as the color that is being checked for
-            if (colorName === buttonTextStatus[buttonNumericalStatus]) {
+            if (colorName === newRoleColor) {
                 member.roles.add(role);
             }
             else {
-                member.roles.remove(role);
+                if (member.roles.cache.has(role.id)) {
+                    member.roles.remove(role);
+                }
             }
         });
 
         // Tell the user that he got the button
-        message.channel.send(`Congratulations, ${message.author.username}! You got a ${buttonTextStatus[buttonNumericalStatus]} belt!`);
-
-        // Reset the Button status
-        clearTimeout(buttonTimeoutLoop);
+        message.channel.send(`Congratulations, ${message.author.username}! You got a ${newRoleColor} belt!`);
+        console.log(`Congratulations, ${message.author.username}! You got a ${newRoleColor} belt!`);
 
         /// Getting a reference to the button status channel
         const buttonStatusChannel: TextChannel = client.channels.cache.get(buttonStatusChannelId) as TextChannel;
 
         const embedMessage: MessageEmbed = new Discord.MessageEmbed()
             .setTitle("**Button Status**")
-            .setDescription(`${message.author.username} has saved the button and earned a ${buttonTextStatus[buttonNumericalStatus]} belt!`)
+            .setDescription(`${message.author.username} has saved the button and earned a ${newRoleColor} belt!`)
             .setColor("WHITE");
 
         buttonStatusChannel.messages.fetch().then((messages) => {
             let messageList: Array<Message> = [...messages.filter(msg => msg.author.id === botId).values()];
-            buttonStatusChannel.send(embedMessage).then(() => {
-                messageList[0].edit(embedMessage);
-
-                /// Resetting the button status
-                buttonNumericalStatus = -1;
-                updateStatus(buttonStatusChannel);
+            buttonStatusChannel.send(embedMessage).then(_ => {
+                messageList[0].edit(embedMessage).then(_ => {
+                    /// Resetting the button status
+                    buttonNumericalStatus = -1;
+                    updateStatus(buttonStatusChannel);
+                });
             });
         });
     }
